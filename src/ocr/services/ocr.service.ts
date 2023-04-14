4b32136eef5c7776823a0dfb071071b73f096a71
nghia-dinh <blackjackptit@gmail.com>
2023-04-14 21:29:46 +0700

    Add not found exception.

--- src/ocr/services/ocr.service.ts
+++ src/ocr/services/ocr.service.ts
@@ -1,13 +1,20 @@
 import {ImageDto} from "../models/dtos/image.dto";
-import {join} from 'path';
+import {join} from "path";
 import {AppConstants} from "../../common/config/constants";
+import {HttpException, HttpStatus} from "@nestjs/common";
 
-const {ocrSpace} = require('ocr-space-api-wrapper');
+const ocrSpace = require("ocr-space-api-wrapper");
+const fs = require("fs");
 
 export class OCRService {
     async hightlight(imageDto: ImageDto) {
+        const filePath = join(process.cwd(), AppConstants.STORAGE_PATH + imageDto.image_name);
+        if (!fs.existsSync(filePath)) {
+            throw new HttpException("File not found", HttpStatus.NOT_FOUND);
+        }
+
         try {
-            return await ocrSpace(join(process.cwd(), AppConstants.STORAGE_PATH + imageDto.image_name), {
+            return await ocrSpace(filePath, {
                 apiKey: process.env.OCR_API_KEY,
                 isOverlayRequired: true,
                 filetype: 'jpg'
